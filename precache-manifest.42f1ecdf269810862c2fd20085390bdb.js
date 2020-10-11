23d925a7969d404dbb47f4e87133487beb0e8f53
snsayed06 <snsayed06@gmail.com>
2020-10-10 22:53:10 -0700

    Updates

---
+++ precache-manifest.42f1ecdf269810862c2fd20085390bdb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7180b12408512b1d3d7c40db0e8bfe25",
+    "url": "/Finance-Application/index.html"
+  },
+  {
+    "revision": "c629c8e83cce5912251f",
+    "url": "/Finance-Application/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1481374f897375ebf933",
+    "url": "/Finance-Application/static/js/2.76b0decc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Finance-Application/static/js/2.76b0decc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c629c8e83cce5912251f",
+    "url": "/Finance-Application/static/js/main.cd526676.chunk.js"
+  },
+  {
+    "revision": "1a2856978c9a346a2497",
+    "url": "/Finance-Application/static/js/runtime-main.c136d961.js"
+  },
+  {
+    "revision": "715313cd4f8b7b826663309b8751bb6b",
+    "url": "/Finance-Application/static/media/svg-1.715313cd.svg"
+  },
+  {
+    "revision": "4a122f73bc839268339ab652e3ec374d",
+    "url": "/Finance-Application/static/media/svg-2.4a122f73.svg"
+  },
+  {
+    "revision": "d9e440a5ed9283a035d51b4cb93663f0",
+    "url": "/Finance-Application/static/media/svg-3.d9e440a5.svg"
+  },
+  {
+    "revision": "494b2abdfb54672b38d5c6135a33eda5",
+    "url": "/Finance-Application/static/media/svg-5.494b2abd.svg"
+  },
+  {
+    "revision": "72ed571599b33a2d2810daa85d57e890",
+    "url": "/Finance-Application/static/media/video.72ed5715.mp4"
+  }
+]);
